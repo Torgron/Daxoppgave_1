b8b1a79996172262c23bd8899f8a9817e6ff9ac6
Tor Grønås <tor.gronas@hotmail.com>
2022-04-18 17:45:20 +0200

    Fullført oppgave
    Lagt til lykketall, lydeffekter, m/m.

--- script.js
+++ script.js
@@ -1,62 +1,88 @@
-        // Your web app's Firebase configuration
-        // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-        var firebaseConfig = {
-            apiKey: "AIzaSyD0P6nfDr4BNkbEHDFVklVe9zjv_8eEcXk",
-            authDomain: "anmeldelser-c1f00.firebaseapp.com",
-            projectId: "anmeldelser-c1f00",
-            storageBucket: "anmeldelser-c1f00.appspot.com",
-            messagingSenderId: "792272160394",
-            appId: "1:792272160394:web:01158f6a858ffb71774f73",
-            measurementId: "G-9273ZPPMKY"
-        };
-        // Initialize Firebase
-        firebase.initializeApp(firebaseConfig);
-        firebase.analytics();
-        let db=firebase.firestore();
-        
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+var firebaseConfig = {
+    apiKey: "AIzaSyD0P6nfDr4BNkbEHDFVklVe9zjv_8eEcXk",
+    authDomain: "anmeldelser-c1f00.firebaseapp.com",
+    projectId: "anmeldelser-c1f00",
+    storageBucket: "anmeldelser-c1f00.appspot.com",
+    messagingSenderId: "792272160394",
+    appId: "1:792272160394:web:01158f6a858ffb71774f73",
+    measurementId: "G-9273ZPPMKY"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+let db = firebase.firestore();
 
-            let form = $("#form");
-            let input = $("#input");
-            let submit = $("#submit");
+let navnForm = $("#navnForm")[0];
+let navnInput = $("#navnInput")[0];
+let navnSubmit = $("#navnSubmit")[0];
 
-            //The form.addEventListener() method attaches an event handler to the form.
-            //"() =>" is a shorter way to write a function syntax.
-            form.addEventListener("submit", (event) => {
-                //The preventDefault() method cancels the event if it is cancelable, meaning that the default action that belongs to the event will not occur.
-                event.preventDefault();
+navnForm.addEventListener("submit", (event) => {
+    event.preventDefault();
 
-            //Creates variable with input data as an object.
-            let docDataReview = {
-                input: input.value
-            };
+    sessionStorage.setItem('inputValue', navnInput.value);
+    let sesNameInput = sessionStorage.getItem('inputValue');
 
-            db.collection("rew").doc().set(docDataReview).then(()=>{
-                console.log("Suksess");
-                window.location.reload();
-            }).catch(err => console.log(err));
-            });
+    $("#welcomeHeader").text("Velkommen, vi ønsker deg alt som er godt, " + sesNameInput + "!");
 
-            let navnListe = [];
+    let docDataReview = {
+        input: navnInput.value
+    };
 
-            function hentData(doc){
-                console.log(navnListe);
-                navnListe.push(doc.data().input);
-                $("#output").text(navnListe);
-                console.log(navnListe);
+    db.collection("rew").doc().set(docDataReview).then(() => {
+        console.log("Suksess");
+    }).catch(err => console.log(err));
 
-                /*
-                let html = document.createElement('li');
-                html.innerHTML += doc.data().input;
-                JSON.stringify(html);
-                let list = document.getElementById("liste");
-                list.appendChild(html);
-                */
-            }
+    navnInput.value = "";
 
-            db.collection("rew").get().then(snapshot => {
-                snapshot.docs.forEach(doc => {
-                    hentData(doc)
-                });
-            }).catch(err => {
-                console.log(err);
-            })
+});
+
+let navnListe = [];
+
+function hentData(doc) {
+    console.log(navnListe);
+    navnListe.push(" " + doc.data().input);
+    $("#navnOutput").text(navnListe);
+    console.log(navnListe);
+}
+
+db.collection("rew").get().then(snapshot => {
+    snapshot.docs.forEach(doc => {
+        hentData(doc)
+    });
+}).catch(err => {
+    console.log(err);
+})
+
+let numberChangeSnd = new Audio("numberChange.wav");
+let colorChangeSnd = new Audio("colorChange.wav");
+
+let luckyNumber;
+
+function newLuckyNumber() {
+    luckyNumber = Math.floor(Math.random() * (10 - 1) + 1);
+    $("#lykketallOutput").text(luckyNumber);
+}
+
+let lastMinute = new Date().getMinutes();
+
+function checkMinute() {
+    let = userNumber = $("#numberInput")[0].value;
+    let currentMinute = new Date().getMinutes();
+    if (currentMinute !== lastMinute) {
+        lastMinute = currentMinute
+        console.log("Minute changed")
+        numberChangeSnd.play();
+        newLuckyNumber();
+        if (userNumber == luckyNumber) {
+            $("body").css("background-color", "#" + Math.floor(Math.random()*16777215).toString(16));
+            colorChangeSnd.play();
+            alert("DETTE ER DIN NYE LYKKEFARGE!");
+        }
+    };
+    setTimeout(checkMinute, 1000);
+};
+
+checkMinute();
+newLuckyNumber();
